b1c5e52ae0b4e878a97e1e649ff978adcfac5008
Ivan Fayzulin <kemsnake@gmail.com>
2014-09-22 02:25:52 +0700

    added commerce_addressbook module

--- sites/all/themes/stepug/js/stepug.behaviors.js
+++ sites/all/themes/stepug/js/stepug.behaviors.js
@@ -78,4 +78,13 @@
         }
     }
 
+    /**
+    * Automatically submit the payment redirect form.
+    */
+    Drupal.behaviors.commercePayment = {
+        attach: function (context, settings) {
+          $('div.payment-redirect-form form', context).submit();
+        }
+    }
+
 })(jQuery);
